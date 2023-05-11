37dc599881ddb0b9047ce318574a99422ade70e4
alkmnd <blvantla@gmail.com>
2023-05-11 08:30:40 -0700

    created app

---
+++ AliasFrontend/AliasFrontend/AliasFrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliasFrontendApp.swift
+//  AliasFrontend
+//
+//  Created by 1234 on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct AliasFrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
