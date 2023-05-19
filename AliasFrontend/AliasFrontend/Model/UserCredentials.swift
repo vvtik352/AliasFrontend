4f1dc4e614f7cdb77f03ed62acd6a14b6864c551
vvtihomirov <vvtik352@gmail.com>
2023-05-19 23:42:33 +0300

    chore (SignUpViewModel): added registration  request

---
+++ AliasFrontend/AliasFrontend/Model/UserCredentials.swift
@@ -0,0 +1,15 @@
+//
+//  UserCredentials.swift
+//  AliasFrontend
+//
+//  Created by Vladimir on 19.05.2023.
+//
+
+import Foundation
+
+struct UserCredentials {
+    var name: String
+    var email: String
+    var password: String
+}
+
--- AliasFrontend/AliasFrontend/SignUpView.swift
+++ AliasFrontend/AliasFrontend/SignUpView.swift
@@ -10,9 +10,56 @@ import SwiftUI
 
 
 struct SignUpView: View {
-    @State private var login = ""
+    
+    @StateObject var signUpVM = SignUpViewModel()
+
+    @State private var name = ""
+    @State private var email = ""
     @State private var password = ""
     @State private var repeatPassword = ""
+    
+    func registerUser() {
+        // ensure that the password fields match
+        guard password == repeatPassword else {
+            // handle non-matching passwords
+            print("Passwords don't match")
+            return
+        }
+        print("kkek")
+        // create the url with URL
+        let url = URL(string: "http://localhost:8080/users/register")!
+
+        // create the post request
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+
+        // Headers
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        // convert parameters to JSON and add to request body
+        let parameters: [String: Any] = ["name": name, "password": password]
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+        } catch let error {
+            print(error.localizedDescription)
+            return
+        }
+
+        // create dataTask using session object to send data to server
+        let task = URLSession.shared.dataTask(with: request, completionHandler: { data, response, error in
+
+            guard error == nil else {
+                print("Error occurred: \(String(describing: error))")
+                return
+            }
+
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+            }
+        })
+
+        task.resume()
+    }
     
     var body: some View {
         ZStack {
@@ -20,17 +67,24 @@ struct SignUpView: View {
                 .foregroundStyle(.linearGradient(colors: [.black, .mint], startPoint: .top, endPoint: .bottomTrailing))
                 .ignoresSafeArea()
             VStack(spacing: 20) {
-                TextField("Login", text: $login)
+                TextField("Name", text: $signUpVM.userCredentials.name)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .padding()
+                    .bold()
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundColor(.white)
+                TextField("Email", text: $signUpVM.userCredentials.email)
                     .foregroundColor(.white)
                     .textFieldStyle(.plain)
                     .padding()
                     .bold()
                 
-                
                 Rectangle()
                     .frame(width: 350, height: 1)
                     .foregroundColor(.white)
-                SecureField("Password", text: $password)
+                SecureField("Password", text: $signUpVM.userCredentials.password)
                     .foregroundColor(.white)
                     .textFieldStyle(.plain)
                     .padding(.leading)
@@ -38,17 +92,8 @@ struct SignUpView: View {
                 Rectangle()
                     .frame(width: 350, height: 1)
                     .foregroundColor(.white)
-                SecureField("Repeat password", text: $repeatPassword)
-                    .foregroundColor(.white)
-                    .textFieldStyle(.plain)
-                    .padding(.leading)
-                    .bold()
-                
-                Rectangle()
-                    .frame(width: 350, height: 1)
-                    .foregroundColor(.white)
-                // add action
                 Button {
+                    signUpVM.registerUser()
                 } label: {
                     Text("Sign up")
                         .bold()
